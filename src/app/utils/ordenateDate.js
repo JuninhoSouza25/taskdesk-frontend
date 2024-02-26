a35c1622592b0fa0ad8aeeb4919e25278c0d217f
JuninhoSouza25 <dnt-bh@hotmail.com>
2024-02-25 21:27:52 -0300

    chore: ordenar data kanban por atualização

--- src/app/utils/ordenateDate.js
+++ src/app/utils/ordenateDate.js
@@ -1,7 +1,7 @@
 export function ordenateDate(array) {
     array.sort(function(a, b) {
-        var dataA = new Date(a.expiry);
-        var dataB = new Date(b.expiry);
+        var dataA = new Date(a.updatedAt);
+        var dataB = new Date(b.updatedAt);
         return dataB - dataA;
     });
     return array;
